c7f691ddb50a79b0374b3f593306e44161f4efaf
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-08-01 17:14:34 +0200

    Create 6_last_digit_of_the_sum_of_fibonacci_numbers .cpp

---
+++ Algorithms-ToolBox/WEEK-2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers .cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+
+int get_fibonacci_last_digit(long long n) {
+
+    vector<int>fib;
+    vector<int>fib0;
+    fib.push_back(0);
+    fib.push_back(1);
+    fib0.push_back(0);
+    fib0.push_back(1);
+    for (int i = 2; i < 60; i++) {
+        int x = fib[i - 1] + fib[i - 2];
+        fib.push_back(x % 10);
+        fib0.push_back(fib0[i - 1] + fib[i]);
+    }
+    int x = n % 60;
+    long long xx = n / 60;
+
+    return (xx * fib0[59] + fib0[x]) % 10;
+}
+
+
+
+int main() {
+
+    long long n,m;
+    std::cin >>m >> n;
+    int c = get_fibonacci_last_digit(n)- get_fibonacci_last_digit(m-1);
+    
+    std::cout << (c+10)%10 << '\n';
+    return 0;
+}
