b4c61404628bf5843e313f53ee167140fea0e92b
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-08-01 17:11:11 +0200

    Create 5_fibonacci_number_again .cpp

---
+++ Algorithms-ToolBox/WEEK-2_algorithmic_warmup/5_fibonacci_number_again .cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+
+long long fibonacci_fast(long long n,int m) {
+
+    vector<long long>fib;
+    fib.push_back(0);
+    fib.push_back(1);
+    for (int i = 2; i <= n; i++) {
+
+        fib.push_back((fib[i - 1] + fib[i - 2])%m);
+    }
+    return fib[n];
+}
+
+int period(vector<int>fib) {
+    int s = fib.size()/2;
+    if (s % 2 != 0) { return 0; }
+    for (int i = 0; i < s ; i++) {
+        if (fib[i] != fib[i + s]) {
+            return 0;
+        }
+    }
+
+    return s;
+}
+
+
+int main() {
+
+    long long n = 0;
+    int m = 0, i = 0;
+    int Xperiod = 0;
+    std::cin >> n >> m;
+
+    vector<int>fib;
+    fib.push_back(0);
+    fib.push_back(1);
+    for (int i = 2; i <= n; i++) {
+        fib.push_back((fib[i - 1] + fib[i - 2]) % m);
+        Xperiod = period(fib);
+        if (Xperiod != 0) {
+            break;
+        }
+    }
+    if(Xperiod==0){ std::cout << fib[n] << '\n'; }
+    else {
+        std::cout << fib[n%Xperiod] << '\n';
+    }
+
+    return 0;
+}
