7df5bac38b0af0628535f4a4c93fc6a5bf49f730
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-08-01 17:27:33 +0200

    Update 8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers .cpp

--- Algorithms-ToolBox/WEEK-2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers .cpp
+++ Algorithms-ToolBox/WEEK-2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers .cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+
+int get_fibonacci_last_digit(long long n) {
+
+    vector<int>fib;
+    vector<int>fib0;
+    fib.push_back(0);
+    fib.push_back(1);
+    fib0.push_back(0);
+    fib0.push_back(1);
+    for (int i = 2; i < 60; i++) {
+        int x = fib[i - 1] + fib[i - 2];
+        fib.push_back(x % 10);
+        fib0.push_back((fib0[i - 1] + fib[i] * fib[i]) % 10);
+    }
+    int x = n % 60;
+    long long xx = n / 60;
+
+    return (xx * fib0[59] + fib0[x]) % 10;
+}
+
+
+
+int main() {
+
+    long long n;
+
+    cin >> n;
+    int c = get_fibonacci_last_digit(n);
+    cout << c << '\n';
+
+    return 0;
+}
 
