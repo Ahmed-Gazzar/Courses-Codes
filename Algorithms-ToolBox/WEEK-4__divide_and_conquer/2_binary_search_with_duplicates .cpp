a9fc818fe4768ff535ddcd50d5a26fc371c121e2
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-08-05 19:52:12 +0200

    Create 2_binary_search_with_duplicates .cpp

---
+++ Algorithms-ToolBox/WEEK-4__divide_and_conquer/2_binary_search_with_duplicates .cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+using std::vector;
+
+
+
+
+
+int binary_search(const vector<int>& a, int x) {
+    int left = 0, right = (int)a.size() - 1;
+    while (true) {
+        int mid = (left + right) / 2;
+
+        if (a[mid] == x) {
+            int res = mid;
+            while (res != 0 && a[res - 1] == x) {
+                res--;
+            }
+            return res;
+        }
+        if (a[mid] != x && right <= left) {
+            return -1;
+        }
+
+        if (x > a[mid]) {
+            left = mid + 1;
+        }
+
+        if (x < a[mid]) {
+            right = mid - 1;
+        }
+
+    }
+
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cin >> a[i];
+    }
+    int k;
+    std::cin >> k;
+    vector<int> b(k);
+    for (int i = 0; i < k; ++i) {
+        std::cin >> b[i];
+    }
+    for (int i = 0; i < k; ++i) {
+        std::cout << binary_search(a, b[i]) << ' ';
+    }
+}
