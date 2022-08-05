9ce4b68a371f20f6b0f38490e0774687b9303890
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-08-05 19:57:57 +0200

    Create 5_longest_common_subsequence_of_three_sequences .cpp

---
+++ Algorithms-ToolBox/WEEK-5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences .cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+using std::vector;
+using std::swap;
+
+int f(vector<vector<int>> res, vector<int> a, vector<int> b) {
+
+	int indel = 0, mismatch = 0;
+	int l1 = a.size();
+	int l2 = b.size();
+
+
+	int i = l1, j = l2;
+	while (i > 0 && j > 0) {
+		if (res[i][j] == (res[i - 1][j] + 1)) {
+			indel++;
+			i--;
+		}
+		else {
+			if (res[i][j] == (res[i][j - 1] + 1)) {
+				indel++;
+				j--;
+			}
+			else {
+				if (a[i - 1] != b[j - 1] && res[i][j] == (res[i - 1][j - 1] + 1)) {
+					mismatch++;
+					i--; j--;
+				}
+				else {
+					if (a[i - 1] == b[j - 1] && res[i][j] == (res[i - 1][j - 1])) {
+						i--; j--;
+
+					}
+				}
+			}
+		}
+	}
+	indel += i + j;
+	int total = l1 + l2;
+	int common2 = total - indel - (2 * mismatch);
+	return common2 / 2;
+}
+vector<int> f2(vector<vector<int>> res, vector<int> a, vector<int> b) {
+
+	int indel = 0, mismatch = 0;
+	int l1 = a.size();
+	int l2 = b.size();
+	vector<int> r;
+
+	int i = l1, j = l2;
+	while (i > 0 && j > 0) {
+		if (res[i][j] == (res[i - 1][j] + 1)) {
+			indel++;
+			i--;
+		}
+		else {
+			if (res[i][j] == (res[i][j - 1] + 1)) {
+				indel++;
+				j--;
+			}
+			else {
+				if (a[i - 1] != b[j - 1] && res[i][j] == (res[i - 1][j - 1] + 1)) {
+					mismatch++;
+					i--; j--;
+				}
+				else {
+					if (a[i - 1] == b[j - 1] && res[i][j] == (res[i - 1][j - 1])) {
+						r.push_back(a[i - 1]);
+						i--; j--;
+					}
+				}
+			}
+		}
+	}
+	indel += i + j;
+	int total = l1 + l2;
+	int common2 = total - indel - (2 * mismatch);
+	reverse(r.begin(), r.end());
+	return r;
+}
+int lcs2(vector<int>& a, vector<int>& b) {
+	int indel = 0, mismatch = 0;
+	int l1 = a.size();
+	int l2 = b.size();
+	vector<vector<int>> res(l1 + 1, vector<int>(l2 + 1, 0));
+	for (int i = 0; i <= l1; i++) {
+		res[i][0] = i;
+	}
+	for (int i = 0; i <= l2; i++) {
+		res[0][i] = i;
+	}
+	for (int i = 1; i <= l1; i++) {
+		for (int j = 1; j <= l2; j++) {
+			if (a[i - 1] == b[j - 1]) {
+				res[i][j] = min(min(res[i - 1][j] + 1, res[i][j - 1] + 1), res[i - 1][j - 1]);
+			}
+			else {
+				res[i][j] = min(min(res[i - 1][j] + 1, res[i][j - 1] + 1), res[i - 1][j - 1] + 1);
+			}
+		}
+	}
+
+	return f(res, a, b);
+}
+vector<int> lcs22(vector<int>& a, vector<int>& b) {
+	int indel = 0, mismatch = 0;
+	int l1 = a.size();
+	int l2 = b.size();
+	vector<vector<int>> res(l1 + 1, vector<int>(l2 + 1, 0));
+	for (int i = 0; i <= l1; i++) {
+		res[i][0] = i;
+	}
+	for (int i = 0; i <= l2; i++) {
+		res[0][i] = i;
+	}
+	for (int i = 1; i <= l1; i++) {
+		for (int j = 1; j <= l2; j++) {
+			if (a[i - 1] == b[j - 1]) {
+				res[i][j] = min(min(res[i - 1][j] + 1, res[i][j - 1] + 1), res[i - 1][j - 1]);
+			}
+			else {
+				res[i][j] = min(min(res[i - 1][j] + 1, res[i][j - 1] + 1), res[i - 1][j - 1] + 1);
+			}
+		}
+	}
+
+	return f2(res, a, b);
+}
+int main() {
+
+	size_t n;
+	std::cin >> n;
+	vector<int> a(n);
+	for (size_t i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+
+	size_t m;
+	std::cin >> m;
+	vector<int> b(m);
+	for (size_t i = 0; i < m; i++) {
+		std::cin >> b[i];
+	}
+
+	size_t o;
+	std::cin >> o;
+	vector<int> c(o);
+	for (size_t i = 0; i < o; i++) {
+		std::cin >> c[i];
+	}
+
+	vector<int> ab = lcs22(a, b);
+	std::cout << lcs2(c, ab) << std::endl;
+
+
+}
